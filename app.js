289170fce0c6ed0ef8140158fd1961a8447b718b
Algimantas Badauskas <algimantas.badauskas@gmail.com>
2025-05-13 15:40:57 +0300

    first commit

--- app.js
+++ app.js
@@ -1,80 +1,89 @@
-const key = "4d7b7fc6f9d244dfb21182039252804";
-const url = "http://api.weatherapi.com/v1/current.json";
-// const city = "Vilnius";
-const temp = document.querySelector("#temp");
-const cityInput = document.querySelector("#city-input");
-const cityName = document.querySelector("#city-name");
-const date = (document.querySelector("#date").textContent =
-  getFormattedToday());
-const humidity = document.querySelector("#humidity");
-const wind = document.querySelector("#wind");
-
-async function getWeather(city) {
-  const response = await fetch(`${url}?key=${key}&q=${city}`);
-  const data = await response.json();
-  return data;
-  console.log(data);
-}
-
-function handleWeatherData(data) {
-  const tempC = data.current.temp_c;
-  const iconUrl = data.current.condition.icon;
-  const humidityValue = data.current.humidity;
-  const windSpeed = data.current.wind_kph;
-  const windDirection = data.current.wind_dir;
-
-  // temp.innerHTML = `${tempC}°C`;
-
-  humidity.innerHTML = `${humidityValue}%`;
-  wind.innerHTML = `${windSpeed} km/h ${windDirection}`;
-  icon.src = iconUrl;
-  cityName.textContent = data.location.name;
-}
-
-getWeather(cityInput.value).then((data) => {
-  handleWeatherData(data);
+const myForm = document.querySelector("form");
+myForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputs = event.target.elements;
+  const lenghtinp = +inputs.lenght.value; //slaptazodzio ilgis
+  const myCheck = inputs.myCheck.checked; //dydziosios
+  const myCheck1 = inputs.myCheck1.checked; //skaiciai
+  const myCheck2 = inputs.myCheck2.checked; //symboliai
+  document.querySelector(
+    "h3"
+  ).innerHTML = `Passw ilgis ${lenghtinp} didziosios ${myCheck} skaiciai ${myCheck1} symboliai ${myCheck2} `;
+  console.log(lenghtinp);
 });
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const city = cityInput.value;
-  // const city = "Vilnius";
-  getWeather(city).then((data) => {
-    handleWeatherData(data);
-    console.log(e);
-  });
-});
+// Generuojam raide
 
-function getFormattedToday() {
-  const date = new Date();
-  const day = date.getDate();
-  const weekday = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(
-    date
-  );
-  const month = new Intl.DateTimeFormat("en-US", { month: "long" }).format(
-    date
-  );
+let letter = ""; //abeceles raidziu skaicius
+let lowerpsw;
+let upperpsw;
+let numberpsw;
+let symbolpsw;
+let result = "";
 
-  function getOrdinal(n) {
-    if (n >= 11 && n <= 13) return n + "th";
-    switch (n % 10) {
-      case 1:
-        return n + "st";
-      case 2:
-        return n + "nd";
-      case 3:
-        return n + "rd";
-      default:
-        return n + "th";
+function getLetterByIndex(index) {
+  const symbol = [
+    "^",
+    "&",
+    "*",
+    ",",
+    ".",
+    "?",
+    ":",
+    "{",
+    "}",
+    "|",
+    "<",
+    ">",
+    "]",
+    "[",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+  ];
+
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  letter = Math.floor(Math.random() * alphabet.length); //atsitiktine raide
+  let num = Math.floor(Math.random() * 10);
+  // generuojam atsitiktinius sk. iki pasirinkto ilgio (lenghtinp) kartu
+  for (let j = 0; j < lenghtinp; j++) {
+    // paimam atsitiktine raide
+    for (let i = 0; i < alphabet.length; i++) {
+      lowerpsw = alphabet[letter].toLowerCase();
+      result = result + lowerpsw;
+      console.log(letter);
     }
+    console.log(result);
   }
-
-  return `${weekday}, ${month} ${getOrdinal(day)}`;
+  return alphabet[index];
+  getLetterByIndex();
 }
-
-function getWeekdayName(dateString) {
-  const date = new Date(dateString);
-  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
-}
-
-console.log(getWeekdayName("2025-05-04")); // Output: "Tuesday"
